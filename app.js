5c4774f4378a55ea2912d692ccb161925b43261a
Rahul Dhakad <dhakad706@gmail.com>
2023-12-21 19:11:47 +0530

    integerated with mongoDB with the help of mongoose

--- app.js
+++ app.js
@@ -1,25 +1,61 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"))
-var all = [];
+
+mongoose.connect("mongodb://localhost:27017/todoDB");
+
+const schema={
+  name:String
+}
+
+const Task=mongoose.model("Task",schema);
+
+
+var allTasks=[];
+
+const getTasks = async function(){
+   const res = await Task.find({});
+   console.log(res)
+   allTasks=[...res];
+}
+
+
+// const getTasks = async function(){
+//   const res = await Task.find({});
+//   allTasks.push(res.name)
+  
+// }
 
 let day = date.getDate();
 
 
 app.get("/", (req, res) => {
-  console.log(day);
+  // console.log(day);
+  getTasks();
+  setTimeout(() => {
 
-  res.render("list", { all: all,day:day });
+    res.render("list", { all: allTasks,day:day });
+  }, 10);
+
 });
 
 app.post("/", (req, res) => {
   var item = req.body.task;
-  all.push(item);
+  if(item==""){
+
+  }
+  else {
+
+    const task= new Task({name:item});
+    task.save();
+   
+  }
 
   res.redirect("/");
 });
